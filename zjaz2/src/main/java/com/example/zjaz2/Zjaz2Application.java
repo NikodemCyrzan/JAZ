25953983cdf5a0d782cb037f16a2c4feddc72527
Nikodem Cyrzan <85363474+NikodemCyrzan@users.noreply.github.com>
2025-03-29 05:35:56 +0100

    init zjaz2

---
+++ zjaz2/src/main/java/com/example/zjaz2/Zjaz2Application.java
@@ -0,0 +1,13 @@
+package com.example.zjaz2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Zjaz2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Zjaz2Application.class, args);
+	}
+
+}
